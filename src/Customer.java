f4e6252fb33bb11aa5da9cf94d7c460a9abf0cf0
Nigelitt <43179051+Nigelitt@users.noreply.github.com>
2018-11-07 21:04:01 +0000

    Customer class

---
+++ src/Customer.java
@@ -0,0 +1,68 @@
+public class Customer {
+    private String firstname;
+    private String surname;
+    private String DOB;
+    private String email;
+    private String address;
+    private String postcode;
+    private int phone;
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
+
