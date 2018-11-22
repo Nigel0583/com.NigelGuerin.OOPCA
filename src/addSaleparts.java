2b4e9dfb6ed3334310c041363f214d6ea8d51a89
Nigelitt <43179051+Nigelitt@users.noreply.github.com>
2018-11-22 12:43:00 +0000

    changing add car sale

---
+++ src/addSaleparts.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class addSaleparts extends JFrame {
+    JFrame frame = new JFrame ( "Login" ) ;
+    JLabel label_1 = new JLabel ( "User" ) ;
+    JLabel label_2 = new JLabel ( "Password" ) ;
+    JTextField tfMake = new JTextField ( ) ;
+    JTextField tfModel = new JTextField ( ) ;
+    JTextField tfRegNo = new JTextField ( ) ;
+    JTextField tfCost = new JTextField ( ) ;
+    JTextField tfYear = new JTextField ( ) ;
+    JTextField tfEmmissions = new JTextField ( ) ;
+    JTextField tfTransmission = new JTextField ( ) ;
+    JTextField tfFuel = new JTextField ( ) ;
+    JTextField tfEngine = new JTextField ( ) ;
+    JTextField tfBattery = new JTextField ( ) ;
+    JTextField tfMotor = new JTextField ( ) ;
+    JButton btRegister = new JButton ( "Register" ) ;
+    JButton brLogin = new JButton ( "Login" ) ;
+    public addSaleparts() {
+
+
+    }
+
+
+}
